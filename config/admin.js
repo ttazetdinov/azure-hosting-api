b74a05a7e56e583730edd3698fae37f21d8431fb
Timur Tazetdinov <ttimur89@gmail.com>
2023-01-07 13:07:59 +0100

    wip: solving deployment issues

--- config/admin.js
+++ config/admin.js
@@ -1,8 +1,8 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: 'tobemodified',
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: 'tobemodified',
   },
 });
